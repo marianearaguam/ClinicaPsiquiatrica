fe0ff65b125e25d60bf623a359805c7c03843262
Carolina <ana.carolina16@aluno.ifsertao-pe.edu.br>
2025-02-22 20:58:18 -0300

    Criação

---
+++ ClinicaPsiquiatrica/src/main/java/com/mycompany/clinicapsiquiatrica/ClinicaPsiquiatrica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicapsiquiatrica;
+
+/**
+ *
+ * @author carlo
+ */
+public class ClinicaPsiquiatrica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
